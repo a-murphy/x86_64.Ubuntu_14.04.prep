48aa10f9a7d3616e9dc6a576498a5be3b73eefae
Avi Cavale <avi@shippable.com>
2017-12-19 12:54:34 +0530

    updating scripts to use utils

--- genBuildImage.sh
+++ genBuildImage.sh
@@ -3,15 +3,12 @@
 export HUB_ORG="374168611083.dkr.ecr.us-east-1.amazonaws.com"
 
 set_context() {
-
-  export IMAGE_NAME=$(echo $CONTEXT | awk '{print tolower($0)}')
   export RES_REPO=$CONTEXT"_repo"
   export RES_IMAGE_OUT=$CONTEXT"_img"
+  export IMAGE_NAME=$(echo $CONTEXT | awk '{print tolower($0)}')
   export TAG_NAME="master"
 
-  export RES_REPO_STATE=$(shipctl get_resource_state $RES_REPO)
-  export RES_REPO_UP=$(shipctl to_uppercase $RES_REPO)
-  export RES_REPO_COMMIT=$(eval echo "$"$RES_REPO_UP"_COMMIT")
+  export RES_REPO_COMMIT=$(shipctl get_resource_version_key "$RES_REPO" "shaData.commitSha")
   export BLD_IMG=$HUB_ORG/$IMAGE_NAME:$TAG_NAME
 
   echo "CONTEXT=$CONTEXT"
@@ -20,16 +17,13 @@ set_context() {
   echo "RES_IMAGE_OUT=$RES_IMAGE_OUT"
   echo "HUB_ORG=$HUB_ORG"
   echo "TAG_NAME=$TAG_NAME"
-  echo "RES_REPO_UP=$RES_REPO_UP"
-  echo "RES_REPO_STATE=$RES_REPO_STATE"
   echo "BLD_IMG=$BLD_IMG"
   echo "BUILD_NUMBER=$BUILD_NUMBER"
   echo "RES_REPO_COMMIT=$RES_REPO_COMMIT"
-
 }
 
 create_image() {
-  pushd $RES_REPO_STATE
+  pushd $(shipctl get_resource_state $RES_REPO)
     echo "Starting Docker build & push for $BLD_IMG"
     sudo docker build -t=$BLD_IMG --pull --no-cache .
     echo "Pushing $BLD_IMG"
@@ -40,9 +34,11 @@ create_image() {
 
 create_out_state() {
   echo "Creating a state file for $RES_IMAGE_OUT"
-  shipctl post_resource_state $RES_IMAGE_OUT versionName $TAG_NAME
-  shipctl put_resource_state $RES_IMAGE_OUT IMG_REPO_COMMIT_SHA $RES_REPO_COMMIT
-  shipctl put_resource_state $RES_IMAGE_OUT BUILD_NUMBER $BUILD_NUMBER
+
+  shipctl post_resource_state_multi "$RES_IMAGE_OUT" \
+  "versionName=$TAG_NAME \
+  IMG_REPO_COMMIT_SHA=$RES_REPO_COMMIT \
+  BUILD_NUMBER=$BUILD_NUMBER"
 }
 
 main() {
